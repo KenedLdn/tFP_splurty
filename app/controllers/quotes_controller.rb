a6cd4f74f8c3597ef506a53f01b40765796509b2
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-10 19:00:24 +0000

    Corrected input form bug

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,11 +3,16 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM()").first
 	end
 
+	def error
+	end
+
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
 			flash[:error] = '<h2>Hmm, strange - It seems like your inputs were somehow invalid.</h2>'
-			redirect_to error_path			
+			redirect_to error_path	
+		else
+			redirect_to root_path
 		end
 	end
 
