5fb2d75d3c0d22d84bc967e8680fca0101fb90f3
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-26 18:21:28 +0000

    [Test driven] Add unique tag to each advice

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,9 @@
 class Quote < ActiveRecord::Base
-	validates :saying, :presence => true, :length => {:minimum => 3, :maximum => 140}
-	validates :author, :presence => true, :length => {:minimum => 2, :maximum => 50}
+  validates :saying, :presence => true, :length => {:minimum => 3, :maximum => 140}
+  validates :author, :presence => true, :length => {:minimum => 2, :maximum => 50}
+
+  def unique_tag
+    initials = self.author.split(" ").collect {|sub_string| sub_string[0]}.join
+    "#{initials}\##{self.id}"
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Ken Ho', :saying => 'Jumping jack for 2 mins')
+    expected = "KH\##{quote.id}"
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
 end
