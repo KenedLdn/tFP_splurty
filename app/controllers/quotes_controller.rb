53c0484617a9c633b6981b2bfc4c1734ea5c07c9
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-26 18:51:55 +0000

    [Test driven] Add quotes show page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,32 +1,39 @@
 class QuotesController < ApplicationController
-	def index
-		@quote = Quote.order("RANDOM()").first
-	end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
 
-	def error
-	end
+  def error
+  end
 
-	def last_quote
-		@quote = Quote.last
-	end
+  def last_quote
+    @quote = Quote.last
+  end
 
 
-	def create
-		@quote = Quote.create(quote_params)
-		if @quote.invalid?
-			flash[:error] = '<h2>Hmm, strange - It seems like your inputs were somehow invalid.</h2>'
-			redirect_to error_path	
-		else
-			redirect_to last_quote_path
-		end
-	end
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<h2>Hmm, strange - It seems like your inputs were somehow invalid.</h2>'
+      redirect_to error_path
+    else
+      redirect_to last_quote_path
+    end
+  end
 
-	def about
-	end
+  def about
+  end
 
-	private
+  def show
+    @quote = Quote.where(:id => params[:id]).first
+    if @quote.blank?
+      render :text => 'Not Found', :status => :not_found
+    end
+  end
 
-	def quote_params
-		params.require(:quote).permit(:saying, :author)
-	end
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
